df24da0678f2de607a705cf8a41059882f648e0b
Arthi Vijay Anand <Arthi Vijay Anand>
2016-09-18 18:17:54 +0530

    added article pages

--- server.js
+++ server.js
@@ -21,6 +21,18 @@ app.get('/ui/van.png', function (req, res) {
   res.sendFile(path.join(__dirname, 'ui', 'van.png'));
 });
 
+app.get('/article-one', function (req, res) {
+  res.send('Article One');
+});
+
+app.get('/article-two', function (req, res) {
+  res.send('Article TWO');
+});
+
+app.get('/article-three', function (req, res) {
+  res.send('Article 3');
+});
+
 var port = 8080; // Use 8080 for local development because you might already have apache running on 80
 app.listen(8080, function () {
   console.log(`IMAD course app listening on port ${port}!`);
