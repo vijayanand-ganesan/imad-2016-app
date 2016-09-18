722b33da45ee5201f323717e7616a379531e66e2
Arthi Vijay Anand <Arthi Vijay Anand>
2016-09-18 18:29:57 +0530

    added html content to articles

--- server.js
+++ server.js
@@ -22,15 +22,15 @@ app.get('/ui/van.png', function (req, res) {
 });
 
 app.get('/article-one', function (req, res) {
-  res.send('Article One');
+  res.sendFile(path.join(__dirname, 'ui', 'article-one.html'));
 });
 
 app.get('/article-two', function (req, res) {
-  res.send('Article TWO');
+  res.sendFile(path.join(__dirname, 'ui', 'article-two.html'));
 });
 
 app.get('/article-three', function (req, res) {
-  res.send('Article 3');
+  res.sendFile(path.join(__dirname, 'ui', 'article-three.html'));
 });
 
 var port = 8080; // Use 8080 for local development because you might already have apache running on 80
